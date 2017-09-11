b70deb8e80aa36add70acf211b259b91faa34587
Yin-Hsia Yen <elsieyen@Yin-Hsias-MacBook-Pro.local>
2017-09-11 09:29:18 -0700

    using java

---
+++ 141.linkedListCycle/Ans.java
@@ -0,0 +1,28 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+// If a graph has a cycle, then it must return to the first node
+// Test case: 
+// [3,2,0,-4] tail connects to node index 1
+
+public class Solution {
+    public boolean hasCycle(ListNode head) {
+        if(head==null) return false;
+        ListNode walker = head;
+        ListNode runner = head;
+        while(runner.next!=null && runner.next.next!=null) {
+            walker = walker.next;
+            runner = runner.next.next;
+            if(walker==runner) return true;
+        }
+    return false;
+    }
+}
