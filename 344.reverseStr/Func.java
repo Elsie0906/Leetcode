fd48e2a7427a2468dbf9389c1c6c656777c52276
Yin-Hsia Yen <elsieyen@yin-hsias-mbp.lan>
2018-01-13 22:40:41 -0800

    java

---
+++ 344.reverseStr/Func.java
@@ -0,0 +1,8 @@
+class Solution {
+    public String reverseString(String s) {
+        
+        String reverse = new StringBuffer(s).reverse().toString();
+        
+        return reverse;
+    }
+}
